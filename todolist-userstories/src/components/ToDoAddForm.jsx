65a165a3b05f482451cfd37721f630231b911531
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-05 18:25:24 -0800

    add basic add form

--- todolist-userstories/src/components/ToDoAddForm.jsx
+++ todolist-userstories/src/components/ToDoAddForm.jsx
@@ -1,13 +1,25 @@
-import Button from "@mui/material/Button";
+import React, { useState } from "react";
+import { FormGroup, Button, Box, TextField, Card } from "@mui/material";
 
 const ToDoAddForm = () => {
+  const [task, setTask] = useState("");
   return (
-    <div>
-      ToDoAddForm{" "}
-      <Button variant="contained" color="primary">
-        Add Task
-      </Button>
-    </div>
+    <form>
+      <Box className="h-screen flex justify-center items-center space-x-3">
+        <TextField
+          id="add-task-textfield"
+          label="New Task"
+          variant="outlined"
+          required
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <Button variant="contained" color="primary">
+          Add Task
+        </Button>
+        <Box>New Task is: {task}</Box>
+      </Box>
+    </form>
   );
 };
 
