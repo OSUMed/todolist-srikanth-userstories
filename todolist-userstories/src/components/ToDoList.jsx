873ed171fa1f609e1b197582d028c93945b7d91c
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-05 20:30:29 -0800

    add logic for to do list state with local storage

--- todolist-userstories/src/components/ToDoList.jsx
+++ todolist-userstories/src/components/ToDoList.jsx
@@ -3,6 +3,15 @@ import ToDoAddForm from "./ToDoAddForm";
 
 const ToDoList = () => {
   const [toDoList, setToDoList] = useState([]);
+  useEffect(() => {
+    const toDoListString = localStorage.getItem("toDoList");
+    setToDoList(JSON.parse(toDoListString) || []);
+  }, []);
+  useEffect(() => {
+    const toDoListJSON = JSON.stringify(toDoList);
+    localStorage.setItem("toDoList", toDoListJSON);
+  }, [toDoList]);
+
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       {toDoList.map((task) => {
