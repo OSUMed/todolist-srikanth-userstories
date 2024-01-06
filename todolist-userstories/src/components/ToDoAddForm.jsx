5bed62459fb8cbc4c6c5ea8df1b1572542592e03
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-05 19:13:32 -0800

    Style error message and component

--- todolist-userstories/src/components/ToDoAddForm.jsx
+++ todolist-userstories/src/components/ToDoAddForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Button, Box, TextField, Card } from "@mui/material";
+import { Button, Box, TextField, Card, CardContent } from "@mui/material";
+import FormHelperText from "@mui/material/FormHelperText";
 
 const ToDoAddForm = () => {
   const [task, setTask] = useState("");
@@ -14,28 +15,44 @@ const ToDoAddForm = () => {
     setTask("");
     setError(false);
   };
-  console.log("the values are: ", error, task);
+
   return (
-    <form onSubmit={handleSubmit} noValidate>
-      <Box className="h-screen flex justify-center items-center space-x-3">
-        <TextField
-          error={error}
-          id="add-task-textfield"
-          label="New Task"
-          variant="outlined"
-          value={task}
-          onChange={(e) => {
-            setTask(e.target.value);
-            setError(false);
-          }}
-          helperText={error ? "Please enter a task" : ""}
-        />
-        <Button type="submit" variant="contained" color="primary">
-          Add Task
-        </Button>
-        <Box>New Task is: {task}</Box>
-      </Box>
-    </form>
+    <Box className="h-screen flex justify-center items-center">
+      <Card>
+        <CardContent>
+          <form onSubmit={handleSubmit} noValidate>
+            <Box>
+              <Box
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+                className="space-x-3"
+              >
+                <TextField
+                  error={error}
+                  id="add-task-textfield"
+                  label="New Task"
+                  variant="outlined"
+                  value={task}
+                  onChange={(e) => {
+                    setTask(e.target.value);
+                    setError(false);
+                  }}
+                />
+                <Button type="submit" variant="contained" color="primary">
+                  Add Task
+                </Button>
+              </Box>
+              {error && (
+                <FormHelperText error={true} className="text-center">
+                  Please enter a task
+                </FormHelperText>
+              )}
+            </Box>
+          </form>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
