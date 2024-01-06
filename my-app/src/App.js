ad79516a04d029c6c93b7e0eb0bab8977c8092e5
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-05 17:53:50 -0800

    first commit
    1. add tailwind css and material UI

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="text-xl  bg-blue-200">Hello World</div>
+        <Button variant="contained" color="primary">
+          Click Me
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
