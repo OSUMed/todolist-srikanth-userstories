c09d405fc06d708af2c579e8a7859ffa8d3c148a
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-06 00:42:43 -0800

    Implement ToDoList skeleton with styled button arrangement and delete functionality
    - Create basic structure of ToDoList component
    - Style and arrange action buttons (update, delete) responsively
    - Add delete functionality for tasks

--- todolist-userstories/src/components/ToDoList.jsx
+++ todolist-userstories/src/components/ToDoList.jsx
@@ -1,8 +1,29 @@
 import React, { useEffect, useState } from "react";
 import ToDoAddForm from "./ToDoAddForm";
+import {
+  List,
+  ListItem,
+  Button,
+  Typography,
+  Card,
+  CardContent,
+  Box,
+  Checkbox,
+  ButtonGroup,
+  IconButton,
+  FormGroup,
+  FormControlLabel,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 const ToDoList = () => {
   const [toDoList, setToDoList] = useState([]);
+  const [finishedTasks, setFinishedTasks] = useState([]);
+
+  // Local Storage Logic:
   useEffect(() => {
     const toDoListString = localStorage.getItem("toDoList");
     setToDoList(JSON.parse(toDoListString) || []);
@@ -12,17 +33,118 @@ const ToDoList = () => {
     localStorage.setItem("toDoList", toDoListJSON);
   }, [toDoList]);
 
+  // CRUD Logic:
+  const handleMoveUpList = (taskId) => {};
+  const handleMoveDownList = (taskId) => {};
+  const handleUpdateTask = (taskId) => {};
+  const handleDeleteTask = (taskId) => {
+    setToDoList((prevToDoList) => {
+      return prevToDoList.filter((task) => task.id !== taskId);
+    });
+  };
+
+  const isTaskCompleted = (taskId) => finishedTasks.includes(taskId);
+  const handleCheckBoxToggle = (taskId) => {
+    // if completed task is toggled again, remove it from the finishedTasks array
+    if (isTaskCompleted(taskId)) {
+      setFinishedTasks((prevFinishedTasks) =>
+        prevFinishedTasks.filter((id) => id !== taskId)
+      );
+      return;
+    }
+    setFinishedTasks((prevFinishedTasks) => [...prevFinishedTasks, taskId]);
+  };
+
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      {toDoList.map((task) => {
-        return (
-          <div key={task.id}>
-            <p>{task.taskName}</p>
-          </div>
-        );
-      })}
-      <ToDoAddForm setToDoList={setToDoList} />
-    </div>
+    <Box className="h-screen flex flex-col justify-center items-center">
+      <Card className="w-[60%] ">
+        <CardContent>
+          <List>
+            {toDoList.map((task) => (
+              <Box className="group" key={task.id}>
+                <ListItem className="group-hover:bg-gray-200 rounded shadow-sm p-2 mb-2 ">
+                  <Box
+                    className="flex flex-grow"
+                    onClick={() => handleCheckBoxToggle(task.id)}
+                  >
+                    <FormGroup>
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            checked={isTaskCompleted(task.id)}
+                            disableRipple
+                            onClick={() => handleCheckBoxToggle(task.id)}
+                          />
+                        }
+                        label={
+                          <Typography
+                            variant="h6"
+                            className={`text-center ${
+                              finishedTasks.includes(task.id)
+                                ? "line-through"
+                                : ""
+                            }`}
+                          >
+                            {task.taskName}
+                          </Typography>
+                        }
+                      />
+                    </FormGroup>
+                  </Box>
+
+                  <Box className="flex items-end">
+                    <Box>
+                      <ButtonGroup
+                        variant="text"
+                        aria-label="text button group"
+                        orientation="vertical"
+                        className="space-y-2"
+                      >
+                        <Button
+                          variant="outlined"
+                          onClick={() => handleUpdateTask(task.id)}
+                        >
+                          <Typography className="hidden md:block">
+                            Update
+                          </Typography>
+                          <EditIcon />
+                        </Button>
+                        <Button
+                          color="error"
+                          variant="outlined"
+                          onClick={() => handleDeleteTask(task.id)}
+                        >
+                          <Typography className="hidden md:block">
+                            Delete
+                          </Typography>
+                          <DeleteIcon />
+                        </Button>
+                      </ButtonGroup>
+                    </Box>
+                    <Box className="h-full flex flex-col">
+                      <IconButton
+                        class="text-gray-400 hover:text-gray-600 focus:text-gray-600 active:text-gray-800 p-2"
+                        onClick={() => handleMoveUpList(task.id)}
+                      >
+                        <ArrowUpwardIcon />
+                      </IconButton>
+                      <IconButton
+                        class="text-gray-400 hover:text-gray-600 focus:text-gray-600 active:text-gray-800 p-2"
+                        onClick={() => handleMoveDownList(task.id)}
+                      >
+                        <ArrowDownwardIcon />
+                      </IconButton>
+                    </Box>
+                  </Box>
+                </ListItem>
+              </Box>
+            ))}
+          </List>
+
+          <ToDoAddForm setToDoList={setToDoList} />
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
