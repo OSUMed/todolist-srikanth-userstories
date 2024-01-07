ae0e29e2399263037a1988ad371053eac11b566b
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-06 16:07:08 -0800

    add close dialog and refactor delete functionality inside it

--- todolist-userstories/src/components/ToDoList.jsx
+++ todolist-userstories/src/components/ToDoList.jsx
@@ -13,21 +13,31 @@ import {
   IconButton,
   FormGroup,
   FormControlLabel,
+  Dialog,
+  DialogActions,
 } from "@mui/material";
-import EditIcon from "@mui/icons-material/Edit";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import PropTypes from "prop-types";
 
 const ToDoList = () => {
   const [toDoList, setToDoList] = useState([]);
   const [finishedTasks, setFinishedTasks] = useState([]);
+  const [openDeleteDialog, setOpenDeleteDialog] = React.useState(false);
+  const [selectedTask, setSelectedTask] = useState(null);
 
   // Local Storage Logic:
   useEffect(() => {
     const toDoListString = localStorage.getItem("toDoList");
+
     setToDoList(JSON.parse(toDoListString) || []);
   }, []);
+
   useEffect(() => {
     const toDoListJSON = JSON.stringify(toDoList);
     localStorage.setItem("toDoList", toDoListJSON);
@@ -36,12 +46,6 @@ const ToDoList = () => {
   // CRUD Logic:
   const handleMoveUpList = (taskId) => {};
   const handleMoveDownList = (taskId) => {};
-  const handleUpdateTask = (taskId) => {};
-  const handleDeleteTask = (taskId) => {
-    setToDoList((prevToDoList) => {
-      return prevToDoList.filter((task) => task.id !== taskId);
-    });
-  };
 
   const isTaskCompleted = (taskId) => finishedTasks.includes(taskId);
   const handleCheckBoxToggle = (taskId) => {
@@ -53,6 +57,23 @@ const ToDoList = () => {
       return;
     }
     setFinishedTasks((prevFinishedTasks) => [...prevFinishedTasks, taskId]);
+  };
+
+  const handleDeleteDialogOpen = (task) => {
+    setSelectedTask(task);
+    setOpenDeleteDialog(true);
+  };
+
+  const handleClose = () => {
+    setOpenDeleteDialog(false);
+  };
+
+  const handleDeleteConfirmation = (task) => {
+    let taskId = task.id;
+    setToDoList((prevToDoList) => {
+      return prevToDoList.filter((task) => task.id !== taskId);
+    });
+    setOpenDeleteDialog(false);
   };
 
   return (
@@ -100,19 +121,16 @@ const ToDoList = () => {
                         orientation="vertical"
                         className="space-y-2"
                       >
-                        <Button
-                          variant="outlined"
-                          onClick={() => handleUpdateTask(task.id)}
-                        >
+                        <Button variant="outlined">
                           <Typography className="hidden md:block">
-                            Update
+                            Priority
                           </Typography>
-                          <EditIcon />
+                          <LabelImportantIcon />
                         </Button>
                         <Button
                           color="error"
                           variant="outlined"
-                          onClick={() => handleDeleteTask(task.id)}
+                          onClick={() => handleDeleteDialogOpen(task)}
                         >
                           <Typography className="hidden md:block">
                             Delete
@@ -144,7 +162,50 @@ const ToDoList = () => {
           <ToDoAddForm setToDoList={setToDoList} />
         </CardContent>
       </Card>
+      <Button variant="outlined" onClick={setOpenDeleteDialog} className="p-4">
+        Open simple dialog
+      </Button>
+      <HandleDialogClose
+        setOpen={setOpenDeleteDialog}
+        open={openDeleteDialog}
+        handleClose={handleClose}
+        handleDeleteConfirmation={handleDeleteConfirmation}
+        task={selectedTask}
+      />
     </Box>
+  );
+};
+
+const HandleDialogClose = (props) => {
+  const { open, setOpen, handleClose, handleDeleteConfirmation, task } = props;
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Delete message</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Are you sure you want to delete this message? This cannot be undone.
+        </DialogContentText>
+        <Card variant="outlined" sx={{ margin: 2, padding: 2 }}>
+          <Typography variant="body2">{task && task.taskName}</Typography>
+        </Card>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={handleClose}
+          sx={{ color: "black", border: "1px solid black" }}
+        >
+          Cancel
+        </Button>
+        <Button
+          color="error"
+          variant="contained"
+          onClick={() => handleDeleteConfirmation(task)}
+          startIcon={<DeleteIcon />}
+        >
+          Delete
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
